a8dd79e969c2fffb9b61ff836208bcc043f938aa
Didoupower <matthieulormeau@gmail.com>
2021-03-03 18:50:00 +0100

    rbm class methods definition

--- code/RBM.py
+++ code/RBM.py
@@ -1,11 +1,17 @@
+import numpy as np
+
 class RBM:
+    """Create an RBM object
+    """    
     def __init__(self, input_size, output_size):
-        self.W = np.
+        self.W = np.random.normal(0, 0.01, (input_size, output_size))
+        self.a = np.zeros(input_size)
+        self.b = np.zeros(output_size)
         
-    def input_output():
+    def input_output(self, input):
 
-    def output_input():
+    def output_input(self, output):
 
-    def train():
+    def train(self, data, num_epochs, batch_size, lr):
 
-    def generate_image():
+    def generate_image(self,num_images, gibbs_num_iter):
