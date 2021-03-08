29c988cd2fa48b05d81bd0df177943736512357c
gouzmi <guillaume.dupont.rm@gmail.com>
2021-03-08 01:28:08 +0100

    dbn functions + test on notebook

--- codes/DNN.py
+++ codes/DNN.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.special import expit
 import math
-from RBM import RBM
+from codes.RBM import RBM
+from codes.utils import reconstruction_error
+from collections import Iterable
 
 
 class DNN:
@@ -48,15 +50,25 @@ class DNN:
         if len(X.shape) != 2 or X.shape[1] != self.input_size:
             raise ValueError(
                 "Input dimensions must be (n_samples, RBM.input_size)")
-
+        
+        init_X = X.copy()
+        
+        # pretraining
         for l in self.layers:
-            l.train(X, batch_size, num_epochs, lr)
+            l.train(X, batch_size, num_epochs, lr, False)
             X = l.input_output(X)
 
-        self.pretrained = True
+        # reconstruction error
+        error = 0
+        for x in init_X:
+            h_L = self.input_output(x)
+            x_tild = self.output_input(h_L)
+            error += reconstruction_error(x, x_tild)
 
-    def retropropagation(self):
-        ...
+        to_print = "Pretraining Complete: Reconstruction Error: {:.7f}".format(error/init_X.shape[0])
+        print(to_print)
+        
+        self.pretrained = True
 
     def input_output(self, input):
         """Compute the output for a certain input
@@ -71,11 +83,70 @@ class DNN:
         ndarray:
             (number of samples, last_layer_size)
         """
+        for l in self.layers:
+            p_h = l.input_output(input)
+            input = (np.random.random_sample(l.output_size) < p_h) * 1
+        return input
+
+    def output_input(self, output):
+        """Compute the input for a certain output
+
+        Parameters
+        ----------
+        output : ndarray
+            (number of samples, output_size of last RBM)
+
+        Returns
+        -------
+        ndarray:
+            (number of samples, input_size)
+        """
+        for l in reversed(self.layers):
+            p_v = l.output_input(output)
+            output = (np.random.random_sample(l.input_size) < p_v) * 1
+        return output
+
+    def retropropagation(self):
         ...
 
-    def generate_image_DBN(self):
-        if self.pretrained:
-            ...
+    def generate_image_DBN(self, num_images, gibbs_num_iter, reshape=None):
+        """Return a list of generated images
+
+        Parameters
+        ----------
+        num_images : int
+            number of images to generate
+        gibbs_num_iter : int
+            number of iterations in the Gibbs sampling step
+
+        Returns
+        -------
+        list[ndarray]
+            list of images of shape input_size or reshape (if reshape != None)
+
+        Raises
+        ------
+        ValueError
+            reshape size must be compatible with self.input_size
+        """
+        generated_images = []
+
+        if reshape is not None:
+            if math.prod(reshape) != self.input_size:
+                raise ValueError(
+                    f"Given reshape {reshape} is incompatible with the input size {self.input_size}.")
+
+        for i in range(num_images):
+            v = (np.random.random_sample(self.input_size) < 0.5) * 1
+
+            for j in range(gibbs_num_iter):
+                h_L = self.input_output(v)
+                v = self.output_input(h_L)
+
+            generated_images.append(
+                v if reshape is None else v.reshape(reshape))
+
+        return generated_images
 
     def test_DNN(self):
         if self.trained:
